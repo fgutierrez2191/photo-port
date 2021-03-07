935ddf2aa77c221fc2cd66a6b987d260145d387c
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-03-06 20:25:07 -0500

    create nav tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,24 +1,43 @@
-import React from 'react';
+// import React from 'react';
 
+// import { render, cleanup } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// //import the component we will be testing 
+// import About from '..';
+
+// //call the cleanup function using the aftereach global function 
+// afterEach(cleanup);
+
+// //use the describe function to declare the component we are testing 
+// describe('About component', () => {
+//     //first test
+//     it('renders', () => {
+//         render(<About />)
+//     });
+
+//     //second test 
+//     it('matches snapshot Dom node structure', () => {
+//         //render about
+//     })
+//     const { asFragment } = render(<About />);
+//     expect(asFragment()).toMatchSnapshot();
+// })
+
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-//import the component we will be testing 
 import About from '..';
 
-//call the cleanup function using the aftereach global function 
 afterEach(cleanup);
 
-//use the describe function to declare the component we are testing 
 describe('About component', () => {
-    //first test
-    it('renders', () => {
-        render(<About />)
-    });
-
-    //second test 
-    it('matches snapshot Dom node structure', () => {
-        //render about
-    })
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
+  });
 })
