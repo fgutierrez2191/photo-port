a20c3687f2dd179324fe26a390474aaebd51e325
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-03-06 19:44:53 -0500

    created about component tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+//import the component we will be testing 
+import About from '..';
+
+//call the cleanup function using the aftereach global function 
+afterEach(cleanup);
+
+//use the describe function to declare the component we are testing 
+describe('About component', () => {
+    //first test
+    it('renders', () => {
+        render(<About />)
+    });
+
+    //second test 
+    it('matches snapshot Dom node structure', () => {
+        //render about
+    })
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+})
